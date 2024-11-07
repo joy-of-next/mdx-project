6e7b0e6e45175b2e9a15abb45e81f7fc4b8c68c3
richhost <hexcy@pm.me>
2024-11-07 11:29:05 +0800

    better loadPost

--- src/helpers/load-post.ts
+++ src/helpers/load-post.ts
@@ -3,9 +3,7 @@ import { allPosts } from "content-collections";
 export const loadPost = (slug: string | string[]) => {
   slug = Array.isArray(slug) ? slug.join("/") : slug;
 
-  return allPosts.find(
-    (post) => replaceUrl(post._meta.filePath) === `${slug}.mdx`
-  );
+  return allPosts.find((post) => replaceUrl(post._meta.path) === `${slug}`);
 };
 
 function replaceUrl(url: string) {
